4a8c9a15e88e2ba8da4eaf2175ac8dec179d6c05
anima <anima7lama@gmail.com>
2021-02-18 16:10:29 +0545

    extended base template in html files/created urls for all the pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,13 @@ from .views import *
 app_name = "home"
 
 urlpatterns = [
-    path('', home, name='home') # home = function name
+    path('', home, name='home'), # home = function name
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('elements', elements, name='elements'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
+    path('blogHome', blogHome, name='blogHome'),
+    path('blogSingle', blogSingle, name='blogSingle'),
 ]
