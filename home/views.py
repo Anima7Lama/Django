fa3d6a7ab57cad288a2f0fffb9390e12b98faff0
anima <anima7lama@gmail.com>
2021-02-19 16:06:23 +0545

    Models created | displayed form value in admin dashboard

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,20 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+    
+        data = Contact.objects.create(
+            name = name, # [models_feild = the variable from contact function from above]
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
+
     return render(request, 'contact.html')
     
 def elements(request):
